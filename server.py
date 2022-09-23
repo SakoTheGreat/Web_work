dc78abf82d122c6b09b249dffc2ba680c33b1701
Andrei Sakovich <sakowlandrei@mail.ru>
2022-09-23 19:19:11 +0200

    Something interesting for me

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
+
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    weather_city = weather_by_city("Warsaw, Poland")
-    title = "Прогноз погоды"
-    if weather_city:
-        weather_text =  f"Температура {weather_city['temp_C']} градусов, ощущается как {weather_city['FeelsLikeC']}"
-    else:
-        weather_text =  "Сервис погоды временно не доступен"
-    return render_template('index.html', page_title=title, weather=weather_text)
+    weather = weather_by_city("Warsaw, Poland")
+    title = "Новости Python"
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 if __name__ == "__main__":
     app.run(debug=True)

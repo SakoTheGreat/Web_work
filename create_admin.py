7ba7372f3742edc2f53c8a6e30a40d922d89d3ae
Andrei Sakovich <sakowlandrei@mail.ru>
2022-10-26 15:26:35 +0200

    create users func

---
+++ create_admin.py
@@ -0,0 +1,27 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app
+from webapp.model import db, User
+
+app = create_app()
+
+with app.app_context():
+    username = input('Write the name')
+
+    if User.query.filter(User.username == username).count():
+        print('User with this name are done yet')
+        sys.exit(0)
+    
+    password1 = getpass('Write the password')
+    password2 = getpass('Repeate the password')
+    if not password1 == password2:
+        print('password is not correct')
+        sys.exit(0)
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password1)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print('User with id {} added'.format(new_user.id))
